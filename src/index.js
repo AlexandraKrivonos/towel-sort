7db153007ec1a087921e1b1226afa628b5a6a6d8
AlexandraKrivonos <sashka.krivonos@gmail.com>
2020-10-21 14:29:06 +0300

    fix: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 
-module.exports = function towelSort (matrix) {
-  return matrix.flat();
+module.exports = function towelSort(matrix) {
+  if(matrix === undefined) return []
+  let arrSort = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0 || i === 0) {
+      console.log('-')
+
+      for (let j = 0; j < matrix[i].length; j++) {
+        arrSort.push(matrix[i][j])
+      }
+
+    } else {
+      matrix[i] = matrix[i].reverse();
+      for (let j = 0; j < matrix[i].length; j++) {
+        arrSort.push(matrix[i][j])
+      }
+    }
+  }
+  return arrSort;
 }
